51a62ecf9c1f5b97420ca756400e35ffe1a09147
Ayush <ayush181101@gmail.com>
2024-12-12 00:04:00 +0530

    coupons

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import productRoutes from "./routes/product.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 
 app.listen(PORT,()=>{
