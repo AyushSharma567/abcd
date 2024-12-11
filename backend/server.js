b008f50b7f4ec62f69893524558b0c3fced8c42a
Ayush <ayush181101@gmail.com>
2024-12-11 21:15:54 +0530

    userAuthentication + cookies

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from"./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app=express();
 const PORT=process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
 
 //console.log(process.env.PORT);
 //authentication
@@ -13,4 +18,5 @@ app.use("/api/auth",authRoutes)
 
 app.listen(PORT,()=>{
     console.log("server is running on http://localhost:" + PORT);
+    connectDB();
 });
